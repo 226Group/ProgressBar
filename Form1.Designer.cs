33bc75b60fc6ff1f63f64251eb986e9ba766da18
Михаил Гаврута <mgavruta06@gmail.com>
2025-03-24 14:49:39 +0300

    конец пары

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 357;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(796, 85);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Progress Bar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,27 @@ namespace Windows_Forms.NET__Async_Csharp__Split_Container__ProgressBar
         {
             InitializeComponent();
         }
+
+        //private const string APP_NAME = «MY_PREDICTOR";
+    private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                for (int j = 0; j < 10000000; j++)
+                    i = 0;
+                progressBar1.Value = i;
+            };
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(3000); // Wait for 3 seconds
+                                        // Invoke the button click on the UI thread
+                this.Invoke((MethodInvoker)delegate {
+                    button1_Click(sender, EventArgs.Empty);
+                });
+            });
+        }
     }
 }
