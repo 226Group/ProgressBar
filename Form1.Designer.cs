0177e99f9dcf96988b23dbb3e481b8ad20231995
sud4084@outlook.com <sud4084@outlook.com>
2025-03-25 14:50:50 +0300

    сделал паралельность + конец пары

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ClickMe = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
@@ -44,11 +46,15 @@
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.ClickMe);
+            // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 357;
+            this.splitContainer1.Size = new System.Drawing.Size(582, 243);
+            this.splitContainer1.SplitterDistance = 192;
             this.splitContainer1.TabIndex = 0;
             // 
             // progressBar1
@@ -56,21 +62,34 @@
             this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.progressBar1.Location = new System.Drawing.Point(0, 0);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(796, 85);
+            this.progressBar1.Size = new System.Drawing.Size(578, 43);
             this.progressBar1.TabIndex = 0;
+            // 
+            // ClickMe
+            // 
+            this.ClickMe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ClickMe.Font = new System.Drawing.Font("Agency FB", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClickMe.Location = new System.Drawing.Point(0, 0);
+            this.ClickMe.Name = "ClickMe";
+            this.ClickMe.Size = new System.Drawing.Size(578, 188);
+            this.ClickMe.TabIndex = 0;
+            this.ClickMe.Text = "Click Me!";
+            this.ClickMe.UseVisualStyleBackColor = true;
+            this.ClickMe.Click += new System.EventHandler(this.ClickMe_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(582, 243);
             this.Controls.Add(this.splitContainer1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Progress Bar";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -82,6 +101,7 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button ClickMe;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,31 +13,63 @@ namespace Windows_Forms.NET__Async_Csharp__Split_Container__ProgressBar
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
         }
 
         //private const string APP_NAME = «MY_PREDICTOR";
-    private void button1_Click(object sender, EventArgs e)
+        async private Task button1_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i < 100; i++)
-            {
-                for (int j = 0; j < 10000000; j++)
-                    i = 0;
-                progressBar1.Value = i;
-            };
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            Task.Run(async () =>
-            {
-                await Task.Delay(3000); // Wait for 3 seconds
-                                        // Invoke the button click on the UI thread
-                this.Invoke((MethodInvoker)delegate {
-                    button1_Click(sender, EventArgs.Empty);
-                });
-            });
+            //MessageBox.Show("1");
         }
+        private CancellationTokenSource cancellationTokenSource;
+
+        private async Task Funny(CancellationToken token)
+        {
+            string[] messages = new string[] {
+        "Загрузка... Пожалуйста, подождите, пока наш кот не закончит свои утренние растяжки.",
+        "Загрузка... Мы ищем потерянные носки. Они могут знать, где находится ваш контент!",
+        "Загрузка... Похоже, наш сервер ушел на кофе. Вернется через 5 минут!",
+        "Загрузка... Мы пытаемся найти кнопку 'Сделать всё быстро'. Она где-то здесь!",
+        "Загрузка... Пожалуйста, не отключайте устройство. Мы находимся в процессе поиска вдохновения.",
+        "Загрузка... Наши данные сейчас на свидании. Надеемся, они вернутся с хорошими новостями!",
+        "Загрузка... Мы проверяем, не заблудились ли наши файлы в интернете."
+    };
+            string text = this.Text;
+            while (!token.IsCancellationRequested)
+            {
+                var r = rnd.Next(messages.Length);
+                this.Text = messages[r];
+                await Task.Delay(2000); // Задержка для смены сообщения
+            }
+            this.Text = text;
+        }
+
+        private async void ClickMe_Click(object sender, EventArgs e)
+        {
+            
+            cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+
+            Task loading = Funny(token);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                await Task.Delay(100);
+                progressBar1.Value = i;
+            }
+
+            // Отменяем задачу Funny
+            cancellationTokenSource.Cancel();
+            await loading; // Ждем завершения задачи
+            progressBar1.Value = 0;
+        }
+
     }
 }
